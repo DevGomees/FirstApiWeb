cdb8137b08c1b35d02ca1b141950faf6647ac01a
Gomes <josuegsj1@gmail.com>
2025-03-20 21:07:49 -0300

    Adicionar arquivos de projeto.

---
+++ FirstApiWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FirstApiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApiWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ProdutoModel> Produtos { get; set; }
+    }
+}
---
+++ FirstApiWeb/Models/ProdutoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstApiWeb.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public int QuantidadeEstoque { get; set; }
+        public string Marca { get; set; } = string.Empty;
+    }
+}
