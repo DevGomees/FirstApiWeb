4c84a07074b41e74c7febed4dd85f3e02ecdb299
Gomes <josuegsj1@gmail.com>
2025-03-22 23:49:59 -0300

    add scalar and parameter id

---
+++ FirstApiWeb/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using FirstApiWeb.Data;
+using FirstApiWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProdutoModel>> BuscarProdutos()
+        {
+            var produtos = _context.Produtos.ToList();
+            return Ok(produtos);
+        }
+        [HttpGet]
+        
+        [Route("{id}")]
+        public ActionResult<ProdutoModel> BuscarProdutosPorId(int id)
+        {
+            var produto = _context.Produtos.Find(id);
+
+            if (produto == null)
+                return NotFound("Registro não localizado!");
+
+            return Ok(produto);
+        }
+        
+    }
+}
--- FirstApiWeb/Program.cs
+++ FirstApiWeb/Program.cs
@@ -1,5 +1,6 @@
 using FirstApiWeb.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
