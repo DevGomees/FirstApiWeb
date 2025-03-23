3e289ca2faa6b321619e6ed4e3380a1e59ca091e
Gomes <josuegsj1@gmail.com>
2025-03-23 19:25:20 -0300

    implementation post,put,delet

--- FirstApiWeb/Controllers/ProdutoController.cs
+++ FirstApiWeb/Controllers/ProdutoController.cs
@@ -22,7 +22,7 @@ namespace FirstApiWeb.Controllers
             return Ok(produtos);
         }
         [HttpGet]
-        
+
         [Route("{id}")]
         public ActionResult<ProdutoModel> BuscarProdutosPorId(int id)
         {
@@ -33,6 +33,53 @@ namespace FirstApiWeb.Controllers
 
             return Ok(produto);
         }
-        
+        [HttpPost]
+        public ActionResult<ProdutoModel> CriarProduto(ProdutoModel produtoModel)
+        {
+            if (produtoModel == null)
+            {
+                return BadRequest("Ocorreu um erro na solicitação!");
+            }
+            _context.Produtos.Add(produtoModel);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(BuscarProdutosPorId), new { id = produtoModel.Id, produtoModel });
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<ProdutoModel> EditarProduto(ProdutoModel produtoModel, int id)
+        {
+            var produto = _context.Produtos.Find(id);
+
+            if (produto == null)
+                return NotFound("Registro não localizado!");
+
+            produto.Nome = produtoModel.Nome;
+            produto.Descricao = produtoModel.Descricao;
+            produto.QuantidadeEstoque = produtoModel.QuantidadeEstoque;
+            produto.Marca = produtoModel.Marca;
+
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public ActionResult<ProdutoModel> DeletarProduto(int id)
+        {
+            var produto = _context.Produtos.Find(id);
+
+            if (produto == null)
+                return NotFound("Registro não localizado!");
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
+
 }
+
+  
+ 
